ad942d3ef184cb9619802839632fbd742d526b88
Usuario <Usuario@10.225.174.153>
2017-10-06 09:45:22 +0100

    Segunda kata de is2 version 5. En un mapa a modo de histograma, almacenamos las frecuencias absoluta de los valores de un vector de enteros llamado data. Eliminamos el bucle fori por uno fore y el if lo cambiamos por una sola sentencia.
    Eliminamos el primer bucle y el mapa, esto los crearemos en una nueva 
    clase llamda histogram con un metodo getHistogram.
    En el main instanciaremos un objeto de esa clase y obtendremos el mapa 
    a traves de él.
    
    Hacemos de Histogram una clase con tipo generico,
    Cambiamos todos los Integer e int por un tipo <T> menos los valores del
    mapa y  en el main declaramos data como Integer en vez de int.
    
    Cambiamos el tipo de data de int a String inicializado a un conjunto de
    nombre, tambien cambiamos en histogr el primer Integer a String y lo 
    mismo en el bucle each.

--- src/Kata2.java
+++ src/Kata2.java
@@ -7,12 +7,12 @@ import java.util.Map;
 public class Kata2 {
     
     public static void main(String[] args) {
-        Integer[] data = {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
+        String[] data = {"Rosa", "Pepe", "Juan", "Pepe", "Pepe", "Rosa"};
         
         Histogram histo = new Histogram(data);
-        Map<Integer, Integer> histogr = histo.getHistogram();
+        Map<String, Integer> histogr = histo.getHistogram();
         
-        for (int key : histogr.keySet()) {
+        for (String key : histogr.keySet()) {
             System.out.println(key + "==>" + histogr.get(key));
         }
         
